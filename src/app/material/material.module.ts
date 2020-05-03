5056f43a1b0d3d196239ba8a83f315e448f3b770
Nimisha Wilson <nimishawilson123@gmail.com>
2020-05-03 11:09:00 +0530

    add angular material as a seperate module

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+const materialComponents = [
+  MatButtonModule
+]
+
+@NgModule({
+  imports: [
+    materialComponents
+  ],
+  exports: [
+    materialComponents
+  ],
+})
+export class MaterialModule { }
