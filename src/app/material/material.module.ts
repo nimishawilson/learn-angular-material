4c8a52f8403dbf391c9ea044acbae26fb34a41a7
Nimisha Wilson <nimishawilson123@gmail.com>
2020-05-13 11:29:04 +0530

    badges

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+
 
 const materialComponents = [
   MatButtonModule,
   MatButtonToggleModule,
-  MatIconModule
+  MatIconModule,
+  MatBadgeModule
 ]
 
 @NgModule({
